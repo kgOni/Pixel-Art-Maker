7c75e8bf2a10a7fdbf4a60564f7746f19fab7a3a
Kevin oni <oni_kg@protonmail.com>
2022-01-12 15:48:41 +0000

    Pixel-Art-Maker-finish

--- designs.js
+++ designs.js
@@ -4,26 +4,23 @@ var color;
 
 // When size is submitted by the user, call makeGrid()
 $('#sizePicker').submit(function (grid) {
-    grid.preventDefault();
     var height = $('#inputHeight').val();
     var width = $('#inputWidth').val();
     makeGrid(height, width);
     // console.log(height, width);
+    grid.preventDefault();
 })
 
-//add Table to Page
 function makeGrid(row, col) {
     $('tr').remove();
     for (var i = 1; i <= row; i++) {
-        $('#pixelCanvas').append('<tr id=table' + i +'></tr>');
+        $('#pixelCanvas').append('<tr></tr>');
         for (var j = 1; j <= col; j++) {
-            $('#table' + i).append('<td></td>');
+            $('tr:last').append('<td></td>');
         }
     }
-
-    $('td').click(function addColor() {
+    $('td').click(function () {
         var color = $('#colorPicker').val();
-
         if ($(this).attr('style')) {
             $(this).removeAttr('style')
         } else {
