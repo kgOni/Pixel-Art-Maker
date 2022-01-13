573471b5b964348bba16dedced8326a1214ce6b8
Kevin oni <oni_kg@protonmail.com>
2022-01-13 17:13:57 +0000

    commands

--- designs.js
+++ designs.js
@@ -1,27 +1,30 @@
-sizePicker.addEventListener("submit", (grid) => {
-    var height = sizePicker.querySelector('#inputHeight').value;
-    var widht = sizePicker.querySelector('#inputWidth').value;
-    makeGrid(height, widht);
-    grid.preventDefault();
-});
+$(document).ready(function () {                                             //waits, until the website is compled loaded 
 
-function makeGrid(row, col) {
-    $('table tr').remove();
-    for (var i = 1; i <= row; i++) {
-        $('table').append('<tr></tr>');
-        for (var j = 1; j <= col; j++) {
-            $('tr:last').append('<td></td>');
+    sizePicker.addEventListener("submit", (grid) => {                       //start after click submit
+        var height = sizePicker.querySelector('#inputHeight').value;        //pick the height for the grid
+        var widht = sizePicker.querySelector('#inputWidth').value;          //pick the width for the grid
+        makeGrid(height, widht);
+        grid.preventDefault();
+    });
+
+    function makeGrid(row, col) {                                           //call function to creat a grid 
+        $('table tr').remove();                                             //remove the old table if there is one
+        for (var i = 1; i <= row; i++) {                                    //push/append tr td to the table element in the html
+            $('table').append('<tr></tr>');
+            for (var j = 1; j <= col; j++) {
+                $('tr:last').append('<td></td>');
+            };
         };
     };
-};
 
-const pixel = document.querySelector('table');
-pixel.addEventListener('click', function (e) {
-    const color = document.querySelector('#colorPicker').value;
-    if ($(e.target).attr('style')) {
-        $(e.target).removeAttr('style')
-    } else {
-        $(e.target).css('background-color', color);
-    }
+    const pixel = document.querySelector('table');                          //pic Element from table 
+    pixel.addEventListener('click', function (e) {
+        const color = document.querySelector('#colorPicker').value;         //pick the color and push it to the click target
+        if ($(e.target).attr('style')) {
+            $(e.target).removeAttr('style')                                 //remove if there is a style attribute
+        } else {
+            $(e.target).css('background-color', color);                     //push/append color to background 
+        }
+    });
+
 });
-
