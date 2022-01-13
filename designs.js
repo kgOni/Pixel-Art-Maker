2f9788addf0b42c30d35e3aee010033b81b896f8
Kevin oni <oni_kg@protonmail.com>
2022-01-13 12:00:40 +0000

    changes

--- designs.js
+++ designs.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {                         //waits, until the side is complied loaded
+
     $('#sizePicker').submit(function (grid) {
         var height = $('#inputHeight').val();           //pick the height for the grid
         var width = $('#inputWidth').val();             //pick the width for the grid 
@@ -6,9 +7,15 @@ $(document).ready(function () {                         //waits, until the side 
         grid.preventDefault();
     });
 
+
+
     function makeGrid(row, col) {
         $('tr').remove();
+
         for (var i = 1; i <= row; i++) {
+
+
+
             $('#pixelCanvas').append('<tr></tr>');
             for (var j = 1; j <= col; j++) {
                 $('tr:last').append('<td></td>');
@@ -16,12 +23,15 @@ $(document).ready(function () {                         //waits, until the side 
             }
         };
 
-        $('.cells').click(function () {
+
+
+        $('td').click(function () {
             var paint = $('#colorPicker').val();        //pick the color for the background
             if ($(this).attr('style')) {
                 $(this).removeAttr('style')
             } else {
                 $(this).attr('style', 'background-color:' + paint);
+                console.log(this)
             }
         });
     };
