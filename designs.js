db6905a077556c18310d8d97b864126ed621f350
Kevin oni <oni_kg@protonmail.com>
2022-01-13 12:16:49 +0000

    next step

--- designs.js
+++ designs.js
@@ -1,38 +1,27 @@
-$(document).ready(function () {                         //waits, until the side is complied loaded
-
-    $('#sizePicker').submit(function (grid) {
-        var height = $('#inputHeight').val();           //pick the height for the grid
-        var width = $('#inputWidth').val();             //pick the width for the grid 
-        makeGrid(height, width);
-        grid.preventDefault();
-    });
-
-
-
-    function makeGrid(row, col) {
-        $('tr').remove();
-
-        for (var i = 1; i <= row; i++) {
-
-
-
-            $('#pixelCanvas').append('<tr></tr>');
-            for (var j = 1; j <= col; j++) {
-                $('tr:last').append('<td></td>');
-                $('td').attr("class", 'cells');
-            }
-        };
-
-
-
-        $('td').click(function () {
-            var paint = $('#colorPicker').val();        //pick the color for the background
-            if ($(this).attr('style')) {
-                $(this).removeAttr('style')
-            } else {
-                $(this).attr('style', 'background-color:' + paint);
-                console.log(this)
-            }
-        });
-    };
+sizePicker.addEventListener("submit", (grid) => {
+    var height = sizePicker.querySelector('#inputHeight').value;
+    var widht = sizePicker.querySelector('#inputWidth').value;
+    makeGrid(height, widht);
+    grid.preventDefault();
 });
+
+function makeGrid(row, col) {
+    $('table tr').remove();
+    for (var i = 1; i <= row; i++) {
+        $('table').append('<tr></tr>');
+        for (var j = 1; j <= col; j++) {
+            $('tr:last').append('<td></td>');
+        };
+    };
+};
+
+const pixel = document.querySelector('table');
+pixel.addEventListener('click', function (e) {
+    const color = document.querySelector('#colorPicker').value;
+    if ($(e.target).attr('style')) {
+        $(e.target).removeAttr('style')
+    } else {
+        $(e.target).css('background-color', color);
+    }
+});
+
